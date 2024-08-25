3cab4444ffb0231836c6f02d1f08741a1f46b797
Thalia Archibald <thalia@archibald.dev>
2024-08-25 11:45:47 -0700

    censoredusername: Lex dialect

--- src/dialects/mod.rs
+++ src/dialects/mod.rs
@@ -1,10 +1,12 @@
 //! Parsing for Whitespace assembly dialects.
 
 mod burghard;
+mod censoredusername;
 mod palaiologos;
 #[expect(dead_code)]
 mod wconrad;
 
 pub use burghard::Burghard;
+pub use censoredusername::CensoredUsername;
 pub use palaiologos::Palaiologos;
 pub use wconrad::WConrad;
