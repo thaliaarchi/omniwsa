274320a3238febcb89d147629c3aee1af0a6105c
Thalia Archibald <thalia@archibald.dev>
2024-08-25 09:01:47 -0700

    scan: Replace ByteScanner with conventionally UTF-8 Scanner

--- src/dialects/wconrad/lex.rs
+++ src/dialects/wconrad/lex.rs
@@ -1,21 +1,21 @@
 //! Lexer for the wconrad Whitespace assembly dialect.
 
 use crate::{
-    lex::{ByteScanner, Lex},
+    lex::{Lex, Scanner},
     tokens::{spaces::EofToken, Token},
 };
 
 /// A lexer for tokens in the wconrad Whitespace assembly dialect.
 #[derive(Clone, Debug)]
 pub struct Lexer<'s> {
-    scan: ByteScanner<'s>,
+    scan: Scanner<'s>,
 }
 
 impl<'s> Lexer<'s> {
     /// Constructs a new lexer for wconrad-dialect source text.
     pub fn new(src: &'s [u8]) -> Self {
         Lexer {
-            scan: ByteScanner::new(src),
+            scan: Scanner::new(src),
         }
     }
 }
@@ -23,14 +23,14 @@ impl<'s> Lexer<'s> {
 impl<'s> Lex<'s> for Lexer<'s> {
     fn next_token(&mut self) -> Token<'s> {
         let scan = &mut self.scan;
-        scan.reset();
+        scan.start_next();
         if scan.eof() {
             return Token::from(EofToken);
         }
-        match scan.next_byte() {
-            b'-' | b'+' | b'0'..=b'9' => todo!(),
-            b'#' => todo!(),
-            b' ' | b'\t' | b'\n' | b'\x0b' | b'\x0c' | b'\r' => todo!(),
+        match scan.next_char_or_replace() {
+            '-' | '+' | '0'..='9' => todo!(),
+            '#' => todo!(),
+            ' ' | '\t' | '\n' | '\x0b' | '\x0c' | '\r' => todo!(),
             _ => todo!(),
         }
     }
