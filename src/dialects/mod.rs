a91ec3d52d05dc21e9cfc5051052c05e837ce810
Thalia Archibald <thalia@archibald.dev>
2024-08-27 14:16:46 -0700

    wsf: Start lexing dialect

--- src/dialects/mod.rs
+++ src/dialects/mod.rs
@@ -3,7 +3,9 @@
 mod burghard;
 mod censoredusername;
 mod palaiologos;
+mod wsf;
 
 pub use burghard::Burghard;
 pub use censoredusername::CensoredUsername;
 pub use palaiologos::Palaiologos;
+pub use wsf::Wsf;
