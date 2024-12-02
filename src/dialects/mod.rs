4b6e4c5c9e1a637fc9b834ddb524fc1c10effe2d
Thalia Archibald <thalia@archibald.dev>
2024-12-02 03:41:05 -0800

    voliva: Lex dialect

--- src/dialects/mod.rs
+++ src/dialects/mod.rs
@@ -3,9 +3,11 @@
 mod burghard;
 mod censoredusername;
 mod palaiologos;
+mod voliva;
 mod wsf;
 
 pub use burghard::Burghard;
 pub use censoredusername::CensoredUsername;
 pub use palaiologos::Palaiologos;
+pub use voliva::Voliva;
 pub use wsf::Wsf;
