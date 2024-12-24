64e167752295ff02d5bf396f3009c0fb93d0b812
Thalia Archibald <thalia@archibald.dev>
2024-12-24 01:23:32 -0800

    palaiologos: Triage failing tests

--- src/tests.rs
+++ src/tests.rs
@@ -1,5 +1,6 @@
 use std::{
     collections::HashSet,
+    error::Error,
     fmt::{self, Debug, Formatter},
     fs::File,
     io::Read,
@@ -10,7 +11,7 @@ use bstr::ByteSlice;
 use glob::glob;
 
 use crate::{
-    dialects::{Burghard, Dialect as _, Palaiologos},
+    dialects::{Burghard, Dialect as _, DialectState, Palaiologos},
     syntax::Pretty,
 };
 
@@ -73,35 +74,76 @@ fn roundtrip_palaiologos() {
 
 #[test]
 fn codegen() {
-    let dialect = Palaiologos::new();
-    let mut src = Vec::new();
-    let mut ws_expect = Vec::new();
-    let mut ws_generated = String::new();
-    let mut fail = false;
-    for path in [
-        "tests/palaiologos/pass/juxtapose.asm",
-        "tests/palaiologos/wild/ws-build-run/rep_putn.asm",
-        "tests/palaiologos/wild/ws-rebuild/copy.bak",
-        "tests/palaiologos/wild/ws-rebuild/slide.bak",
-    ] {
-        let path = Path::new(path);
+    #[track_caller]
+    fn test(
+        dialect: &DialectState<Palaiologos>,
+        path: &str,
+        src: &mut Vec<u8>,
+        ws_expect: &mut Vec<u8>,
+        ws_generated: &mut String,
+    ) -> Result<bool, Box<dyn Error>> {
+        let path = Path::new("tests/palaiologos").join(path);
         src.clear();
-        File::open(&path).unwrap().read_to_end(&mut src).unwrap();
+        File::open(&path)?.read_to_end(src)?;
         ws_expect.clear();
-        File::open(path.with_extension("ws"))
-            .unwrap()
-            .read_to_end(&mut ws_expect)
-            .unwrap();
+        File::open(path.with_extension("ws"))?.read_to_end(ws_expect)?;
         let cst = dialect.parse(&src);
         ws_generated.clear();
-        cst.codegen(&mut ws_generated, &HashSet::new()).unwrap();
+        cst.codegen(ws_generated, &HashSet::new())?;
         if ws_generated.as_bytes() != ws_expect {
             println!(
                 "parse({path:?}).codegen()\n generated = {:?}\n    expect = {:?}",
                 DebugStl(ws_generated.as_bytes()),
                 DebugStl(&ws_expect),
             );
-            fail = true;
+            Ok(false)
+        } else {
+            Ok(true)
+        }
+    }
+
+    let dialect = Palaiologos::new();
+    let mut src = Vec::new();
+    let mut ws_expect = Vec::new();
+    let mut ws_generated = String::new();
+    let mut fail = false;
+    for path in [
+        "pass/juxtapose.asm",
+        "pass/regress/no_final_lf.asm",
+        "wild/ws-build-run/rep_putn.asm",
+        "wild/ws-rebuild/copy.bak",
+        "wild/ws-rebuild/slide.bak",
+        //// Instruction overloads not resolved
+        // "pass/integer_bounds/value_-2^31+1.asm",
+        // "pass/integer_bounds/value_-2^31.asm",
+        // "pass/integer_bounds/value_2^31-1.asm",
+        // "pass/regress/ignored_line_comment.asm",
+        // "wild/ws-build-run/divc.asm",
+        // "wild/ws-rebuild/halve.bak",
+
+        //// CharToken not handled in WsaInst::integer
+        // "pass/char_escapes.asm",
+        // "wild/ws-build-run/rep_putc.asm",
+
+        //// Negative Integer::to_usize in WsaInst::codegen case PalaiologosRep
+        // "pass/integer_bounds/rep_-2^31+1.asm",
+        // "pass/integer_bounds/rep_-2^31.asm",
+        // "pass/integer_bounds/rep_negative.asm",
+
+        //// Text labels not handled
+        // "pass/regress/label_sort_freq.asm",
+        // "pass/regress/label_sort_unstable.asm",
+        // "wild/ws-build-run/mmltz.asm",
+        // "wild/ws-rebuild/binary.bak",
+    ] {
+        println!("{path}");
+        match test(&dialect, path, &mut src, &mut ws_expect, &mut ws_generated) {
+            Ok(true) => {}
+            Ok(false) => fail = true,
+            Err(err) => {
+                println!("{path}: {err:?}");
+                fail = true;
+            }
         }
     }
     if fail {
