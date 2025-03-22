f8a3814dfc31004bc87af82b2e0d462415880c64
Thalia Archibald <thalia@archibald.dev>
2025-03-21 23:49:32 -0700

    tests: Test more of int(str) grammar

--- tests/langs/python3/int_parse.py
+++ tests/langs/python3/int_parse.py
@@ -2,19 +2,74 @@ import platform
 
 print(f"Python {platform.python_version()}")
 
-print("Digit values in int(str):")
+def value_before_space(c):
+  try:
+    n = int(f"{chr(c)} 1")
+  except:
+    return None
+  if n == -1:
+    return "negative"
+  assert n == 1
+  return "space"
 
-for c in range(0, 0x110000):
-  n = None
+def value_before_digit(c):
+  try:
+    n = int(f"{chr(c)}2")
+  except:
+    return None
+  if n == -2:
+    return "negative"
+  x, y = divmod(n, 10)
+  assert y == 2
+  return x
+
+def value_between_digits(c):
   try:
     n = int(f"1{chr(c)}3")
   except:
-    continue
+    return None
   xy, z = divmod(n, 10)
   assert z == 3
   if xy == 1:
-    print(f"U+{c:04X}", chr(c), "underscore")
-    continue
+    return "underscore"
   x, y = divmod(xy, 10)
   assert x == 1
-  print(f"U+{c:04X}", chr(c), y)
+  return y
+
+def value_after_digit(c):
+  try:
+    n = int(f"1{chr(c)}")
+  except:
+    return None
+  if n == 1:
+    return "space"
+  x, y = divmod(n, 10)
+  assert x == 1
+  return y
+
+def value_after_space(c):
+  try:
+    n = int(f"1 {chr(c)}")
+  except:
+    return None
+  assert n == 1
+  return "space"
+
+for c in range(0, 0x110000):
+  before_space = value_before_space(c)
+  before_digit = value_before_digit(c)
+  between_digits = value_between_digits(c)
+  after_digit = value_after_digit(c)
+  after_space = value_after_space(c)
+  if before_digit == None and between_digits == None and after_digit == None \
+      and before_space == None and after_space == None:
+    continue
+  if before_digit == between_digits and before_digit == after_digit \
+      and before_space == None and after_space == None:
+    value = before_digit
+  elif before_digit == 0 and between_digits == None and after_digit == "space" \
+      and before_space == "space" and after_space == "space":
+    value = "space"
+  else:
+    value = (before_space, before_digit, between_digits, after_digit, after_space)
+  print(f"U+{c:04X}", chr(c), value)
